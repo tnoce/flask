c0aee8e527e0de2db92739f634b5b81001ecd645
長岡周 <volkjajaja@gmail.com>
2019-08-09 06:38:20 +0900

    auto commit

--- eigo/main.py
+++ eigo/main.py
@@ -1,10 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
+import pandas as pd
+import sqlite3
+
 app = Flask(__name__)
 
+conn = sqlite3.connect('database.sqlite')
+c = conn.cursor()
 
-@app.route("/", methods=["GET", "POST"])
+def count_word(text):
+    txts = text
+    result = []
+    tmp = ""
+    dic = {}
+    
+    for x in txts:
+        if x != " ":
+            tmp += x
+        else:
+            chikan = tmp.replace('\n', '').replace('(', '').\
+            replace(')', '').replace('[', '').replace(']', '').\
+            replace('.', '').replace(',', '').replace(':', '').\
+            replace('-', '').replace('?','').replace('!','')
+            
+            result.append(chikan.lower())
+            tmp = ""
+            
+    for word in result:
+        if not word in dic:
+            dic[word] = 1
+        else:
+            dic[word] += 1
+    
+    s = pd.Series(dic)
+    
+    for k, v in sorted(s.items(), key=lambda x: -x[1]):
+        print(str(k) + "：" + str(v))    
+
+
+@app.route("/", methods=["GET","POST"])
 def main_page():
-    return render_template("index.html")
+    message = 'sample_string'
+    total = ''
+
+    if request.method == 'POST':
+        text = request.form['text']
+        total = count_word(text)
+        return render_template("index.html", text=text, total=total)
+
+    return render_template("index.html", message=message, total=total)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8888)
+
+
+
